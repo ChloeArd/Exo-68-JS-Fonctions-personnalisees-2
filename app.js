02705f4275ed7691ee2c5e07e42c2634ff196f2e
ChloeArd <chloe.ardoise@gmail.com>
2020-12-02 16:56:36 +0100

    Exercice 68

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let elements = document.getElementsByTagName('p');
 
+for(let i = 0; i < 11; i++) {
+    elements = document.getElementsByTagName('p').innerHTML = aleatoire().toString();
+    console.log(elements);
+}
+
 function aleatoire() {
     let random = Math.random() * 100;
     random = Math.ceil(random);
     return random;
 }
 
-for( let i = 0; i < 11; i++) {
-    elements = document.getElementsByTagName('p').innerHTML = aleatoire().toString();
-    console.log(elements);
-}
